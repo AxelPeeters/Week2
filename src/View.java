ad32682d5fa7b44c5f94f0f3b556c7f75a341c6c
AxelPeeters <axel.peeters@student.kdg.be>
2019-02-05 11:02:41 +0100

    Push van project

--- src/View.java
+++ src/View.java
@@ -1,2 +1,19 @@
-public class View {
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.*;
+
+public class View extends BorderPane{
+
+
+    public View() {
+        this.initialiseNodes();
+        this.layoutNodes();
+    }
+
+    private void initialiseNodes(){
+
+    }
+
+    private void layoutNodes() {
+
+    }
 }
